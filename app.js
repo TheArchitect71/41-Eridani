8c3fd502aafa9edb38b5eaa1d590553a96b9d01c
Erick Flores <thearchitect@MacBook-Air.local>
2020-08-07 13:02:21 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
+
+mongoose
+  .connect(
+    `mongodb+srv://theArchitect71:${process.env.MONGO_ATLAS_PW}@cluster0-jsigs.mongodb.net/40-EridaniDatabase?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("images")));
+
+app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
+
+module.exports = app;
